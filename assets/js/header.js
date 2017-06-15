d7f2ef8aaa34e39a0e1fd67af18c3d0f5f329d6e
Gopi Raja <gopi@inkmonk.com>
2017-06-15 23:12:36 +0530

    menu UI fix for mobile

--- assets/js/header.js
+++ assets/js/header.js
@@ -2,9 +2,19 @@ $(document).ready(function() {
   $(document).scroll(function() {
     var y = $(this).scrollTop();
     if (y > 250) {
-      $('.navbar-fixed-top').addClass('scrolled');
+      $('.navbar-fixed-top.hidden-xs').addClass('scrolled');
     } else {
-      $('.navbar-fixed-top').removeClass('scrolled');
+      $('.navbar-fixed-top.hidden-xs').removeClass('scrolled');
     }
+  });
+
+  $('.navbar-fixed-top.visible-xs .slide-menu > .fa-navicon').click(function() {
+    $(this).next('.slide-menuContainer').addClass('visible');
+  });
+  $('.slide-menuContainer .fa-close').click(function() {
+    $(this).parents('.slide-menuContainer').removeClass('visible');
+  });
+  $('.heroMenu > ul > li a').click(function() {
+    $(this).next().slideToggle('fast');
   });
 });
